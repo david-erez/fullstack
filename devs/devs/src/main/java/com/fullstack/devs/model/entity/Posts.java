2d174495db554f97906a572e203565807b9dd2e3
david-erez <erezsantiago25@gmail.com>
2026-05-29 16:11:55 -0500

    feat: add userRepository

--- devs/devs/src/main/java/com/fullstack/devs/model/entity/Posts.java
+++ devs/devs/src/main/java/com/fullstack/devs/model/entity/Posts.java
@@ -1,11 +1,11 @@
 package com.fullstack.devs.model.entity;
 
+import com.fullstack.devs.modules.user.model.entity.Users;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
 
 import java.time.LocalDateTime;
 
